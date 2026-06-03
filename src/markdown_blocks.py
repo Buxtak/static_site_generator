7e059fdeda54c9773e3b744e8a4d2e8e92075a1d
Buxtak <buxtak@gmail.com>
2026-06-03 14:35:15 -0500

    M: implement block_to_block_type with BlockType enum and unit tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     final_blocks = []
@@ -7,3 +10,42 @@ def markdown_to_blocks(markdown):
             final_blocks.append(blocks[i])
 
     return final_blocks
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(block_node):
+    lines = block_node.split("\n")
+    index = 0
+    if block_node.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if lines[0].startswith("```") and lines[-1].startswith("```") and len(lines) > 1:
+        return BlockType.CODE
+
+    if block_node.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+
+    if block_node.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+
+    if block_node.startswith("1. "):
+        for line in lines:
+            index += 1
+            if not line.startswith(f"{index}. "):
+                return BlockType.PARAGRAPH
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import BlockType, block_to_block_type, markdown_to_blocks
 
 
 class TestBlockNode(unittest.TestCase):
@@ -23,3 +23,33 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_heading_block(self):
+        md = "## heading 2"
+        block = block_to_block_type(md)
+        self.assertEqual(block, BlockType.HEADING)
+
+    def test_code_block(self):
+        md = "```\n line of code \n```"
+        block = block_to_block_type(md)
+        self.assertEqual(block, BlockType.CODE)
+
+    def test_quote_block(self):
+        md = "> quote 1\n> quote 2\n> quote 3"
+        block = block_to_block_type(md)
+        self.assertEqual(block, BlockType.QUOTE)
+
+    def test_unordered_block(self):
+        md = "- list element 1\n- list element 2\n- list element 3"
+        block = block_to_block_type(md)
+        self.assertEqual(block, BlockType.UNORDERED_LIST)
+
+    def test_ordered_block(self):
+        md = "1. numbered element 1\n2. numbered element 2\n3. numbered element 3"
+        block = block_to_block_type(md)
+        self.assertEqual(block, BlockType.ORDERED_LIST)
+
+    def test_paragraph_block(self):
+        md = "1. numbered element 1\nparagraph continuation just because"
+        block = block_to_block_type(md)
+        self.assertEqual(block, BlockType.PARAGRAPH)
