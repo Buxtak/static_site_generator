56b2bd18194c37f46c58738fc53fd70bf086938c
Buxtak <buxtak@gmail.com>
2026-06-10 15:54:17 -0500

    O: implement cleaner and copier for recursive static file copying

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType
 
 
 def main():
-    dummy = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(dummy)
+    cleaner("public")
+    copier("static", "public")
+
+
+def cleaner(path_public):
+    if os.path.exists(path_public):
+        shutil.rmtree(path_public)
+    os.mkdir(path_public)
+
+
+def copier(path_static, path_public):
+    for filename in os.listdir(path_static):
+        from_path = os.path.join(path_static, filename)
+        dest_path = os.path.join(path_public, filename)
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            os.mkdir(dest_path)
+            copier(from_path, dest_path)
 
 
 main()
